7347013769524c71ac462c2ac48e316e6088f368
ekam <ekam@netflix.com>
2025-02-01 17:17:52 -0800

    done dealer deal card

--- src/model/CardDistributor.ts
+++ src/model/CardDistributor.ts
@@ -23,4 +23,24 @@ export class CardDistributor {
     }
     return deck;
   }
+
+  public shuffle(): void {
+    for (let i = 0; i < this.deck.length; i++) {
+      const j = Math.floor(Math.random() * this.deck.length);
+      const temp = this.deck[i];
+      this.deck[i] = this.deck[j];
+      this.deck[j] = temp;
+    }
+    this.currentIndex = 0;
+  }
+
+  public dealCard(): Card {
+    return this.deck[this.currentIndex++];
+  }
+
+  public ifCutCardReachedThenShuffle(): void {
+    if (this.currentIndex >= this.deck.length * this.cutOff) {
+      this.shuffle();
+    }
+  }
 }
