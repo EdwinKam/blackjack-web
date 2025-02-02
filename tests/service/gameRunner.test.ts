cb6c15521e6d7789f95deda7cf9ec8e1d1dcd5ab
ekam <ekam@netflix.com>
2025-02-01 23:41:09 -0800

    more more test

--- tests/service/gameRunner.test.ts
+++ tests/service/gameRunner.test.ts
@@ -96,12 +96,31 @@ describe("runGame", () => {
     expect(result.playerWin).toBe(2); // Player wins both hands
   });
 
+  it("should result in a player split and 1win1lost", () => {
+    cardDistributor.dealCard
+      .mockReturnValueOnce(new Card(8)) // Player 8
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
+      .mockReturnValueOnce(new Card(8)) // Player 8
+      .mockReturnValueOnce(new Card(7)) // Dealer 7
+      .mockReturnValueOnce(new Card(10)) // First hand gets 10
+      .mockReturnValueOnce(new Card(5)); // Second hand gets 10
+
+    (getPlayAction as jest.Mock)
+      .mockReturnValueOnce(BlackjackAction.Split) // Player splits
+      .mockReturnValueOnce(BlackjackAction.Stand) // First hand stands
+      .mockReturnValueOnce(BlackjackAction.Stand); // Second hand stands
+
+    const result: GameResult = runGame(cardDistributor);
+
+    expect(result.playerWin).toBe(0); // Player wins both hands
+  });
+
   it("should result in a player hit and win", () => {
     cardDistributor.dealCard
       .mockReturnValueOnce(new Card(10)) // Player 10
-      .mockReturnValueOnce(new Card(10)) // Dealer 6
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
       .mockReturnValueOnce(new Card(5)) // Player 5
-      .mockReturnValueOnce(new Card(10)) // Dealer 9
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
       .mockReturnValueOnce(new Card(6)); // Player hits and gets 6
 
     (getPlayAction as jest.Mock)
@@ -111,5 +130,35 @@ describe("runGame", () => {
     const result: GameResult = runGame(cardDistributor);
 
     expect(result.playerWin).toBe(1); // Player wins
+  });
+
+  it("should result in a player double and win", () => {
+    cardDistributor.dealCard
+      .mockReturnValueOnce(new Card(10)) // Player 10
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
+      .mockReturnValueOnce(new Card(10)) // Player 10
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
+      .mockReturnValueOnce(new Card(1)); // Player double and gets 1
+
+    (getPlayAction as jest.Mock).mockReturnValueOnce(BlackjackAction.Double); // Player hits
+
+    const result: GameResult = runGame(cardDistributor);
+
+    expect(result.playerWin).toBe(2); // Player wins
+  });
+
+  it("should result in a player double and win", () => {
+    cardDistributor.dealCard
+      .mockReturnValueOnce(new Card(5)) // Player 5
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
+      .mockReturnValueOnce(new Card(5)) // Player 5
+      .mockReturnValueOnce(new Card(10)) // Dealer 10
+      .mockReturnValueOnce(new Card(2)); // Player double and gets 2
+
+    (getPlayAction as jest.Mock).mockReturnValueOnce(BlackjackAction.Double); // Player hits
+
+    const result: GameResult = runGame(cardDistributor);
+
+    expect(result.playerWin).toBe(-2); // Player lost
   });
 });
