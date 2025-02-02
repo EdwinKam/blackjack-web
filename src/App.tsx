d27d59684be27d4923d8f9bfb8ce4fd08859970d
ekam <ekam@netflix.com>
2025-02-01 16:42:26 -0800

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import startSimulation from "./service/simulator";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const callSimulation = () => {
+    startSimulation();
+  };
 
   return (
     <>
@@ -17,6 +22,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <button onClick={() => callSimulation()}>start something</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/service/simulator.ts
@@ -0,0 +1,3 @@
+export default function startSimulation() {
+  console.log("Simulation started");
+}
